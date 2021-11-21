159015f228514db42e822f68f04613c9f4eea230
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2021-11-21 11:03:09 -0800

    Update level_points.js

--- point_calculator_stuff/level_points.js
+++ point_calculator_stuff/level_points.js
@@ -7,7 +7,7 @@ let lol = (level) => {
       }else if(counter-1 >= 50 && counter-1 < 100){
         score = 50.0 / (Math.pow(Math.E, 0.01 * counter)) * Math.log((210 / (Math.pow(counter, 1.001))));
       }else{
-        score = 50.0 / (Math.pow(Math.E, 0.01 * counter)) * Math.log((3.3 / (Math.pow(counter, .1))));
+        score = 50.0 / (Math.pow(Math.E, 0.01 * counter)) * Math.log((3.3 / Math.pow(counter, .1)));
       }
       return score
 }
