54fdf30000fa95741fd9abb4ddf64f22df51379c
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2021-11-21 08:37:21 -0800

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js")
 module.exports = {
     name: "interactionCreate",
     async execute(interaction) {
-        if(!interaction.isCommand() || !interaction.channel.permissionsFor(interaction.client.user.id).has("SEND_MESSAGES")) return
+        if(!interaction.isCommand() || !interaction.channel.permissionsFor(interaction.client.user.id).has("USE_APPLICATION_COMMANDS")) return
 	const command = interaction.client.commands.get(interaction.commandName)
 	if(!command) return;
 	try {
