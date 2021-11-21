fdf7e5edc36b15dd481783c8ba92b242d018ff7d
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2021-11-20 22:35:47 -0800

    Update leaderboard.js

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -18,11 +18,12 @@ module.exports = {
             await interaction.reply({content: "Please enter a valid profile!", ephemeral: true})
         } else {
             var counte = 0
-           /* var far = []
+            var far = []
             for(let key in leaderboard) {
+                var df = point(key)
                 var obj = {
                     name: key,
-                    points: point(key)
+                    points: df
                 }
                 far.push(obj)
             }
@@ -32,7 +33,7 @@ module.exports = {
                     counte = i+1
                     break;
                 }
-            }*/
+            }
             var ku = 0
             var uk = 0
             var gay = interaction.options.getString("user")
