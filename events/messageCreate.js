058269ea5ded82629e896bf034a27bf258ad9872
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2021-11-21 00:34:26 -0800

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -11,7 +11,7 @@ module.exports = {
          if(message.content.toLowerCase().includes("360")) {
             message.reply("All y'all high refresh players who think Sonic Wave is easier then Erebus, first of all shut the fuck up and 2nd of all of the wave plays by itself for you guys.")
         } 
-        if(message.content.toLowerCase().includes("fluked")) {
+        if(message.content.toLowerCase().includes("fluke")) {
             message.reply("https://media.discordapp.net/attachments/396433111245914112/832804446081122304/Fluker_is_talking.gif")
         }
     }
