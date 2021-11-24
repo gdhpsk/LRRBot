4637812f10f76c0dd1ace5609fe328cc4ccb4454
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2021-11-24 00:16:05 -0800

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,20 +1,12 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
+const lolxd = require("../levels_fetch_api")
 
 module.exports = {
     data: new SlashCommandBuilder()
     .setName("test")
-    .setDescription("Rolls a number between 1-100 by default")
-    .addIntegerOption((option) =>
-        option
-        .setName("number")
-        .setDescription("between 1 and what number do you wanna roll")
-        .setRequired(false)
-    ),
+    .setDescription("testing something out"),
     async execute(interaction, Discord, client) {
         var generate = 100
-        if(interaction.options.getInteger("number")) {
-            generate = interaction.options.getInteger("number")
-        }
         interaction.reply((Math.floor(Math.random() * generate) + 1).toString())
     }
 } 
