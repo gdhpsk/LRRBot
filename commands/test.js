628a0c099c4acbde6c9a50de6d2069226330186b
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2021-11-24 00:07:00 -0800

    k

---
+++ commands/test.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder } = require("@discordjs/builders")
+const api = require("../levels_fetch_api")
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("test")
+    .setDescription("testing api fetch"),
+    async execute(interaction, Discord, client) {
+        interaction.reply(obj["Aronia"])
+    }
+}
