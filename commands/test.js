f2ac0bb196ca38024c651446efba84adb6c74460
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2021-11-24 00:20:45 -0800

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
-const lolxd = require("../levels_getting")
+const lolxd = require("../JSON/levels.json")
 
 module.exports = {
     data: new SlashCommandBuilder()
