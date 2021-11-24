31c78ebd440b2dd7f810b2766046a28ec548f0ea
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2021-11-24 00:28:37 -0800

    Revert "Delete levels_getting.js"
    This reverts commit eb7f925d6f847433d1b8d85c402f40883ad19ab4.

---
+++ levels_getting.js
@@ -0,0 +1,22 @@
+
+const levels = require("./JSON/levels.json")
+var obj = {}
+for(let i = 0; i < Object.keys(levels).length; i++) {
+    var objectval = Object.values(levels)[i]
+    var txt = [`${objectval.minimumPercent}`, `${objectval.progresses}`]
+    if(!objectval.minimumPercent) {
+        txt[0] = false
+    }
+    if(!objectval.progresses) {
+        txt[1] = false
+    }
+    obj[objectval.name] += {
+        name: objectval.name,
+        ytcode: objectval.ytcode,
+        minimumPercent: txt[0],
+        publisher: objectval.publisher,
+        list: objectval.list,
+        progresses: txt[1]
+    }
+}
+module.exports = "LOL"
