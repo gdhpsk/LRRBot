26d6535c4ad7c35c05ebe90eb0acc27d63a421a3
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2021-11-24 14:05:32 -0800

    Update testing.js

--- commands/testing.js
+++ commands/testing.js
@@ -2,6 +2,7 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 
 module.exports = {
     data: new SlashCommandBuilder()
+    .setDefaultPermission(true)
     .setName("testing")
     .setDescription("testing"),
     async execute(interaction, Discord, client) {
