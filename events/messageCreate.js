7012f8c54d859e88aa6ff91d05cfa9734b131858
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2021-11-24 00:30:47 -0800

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -21,7 +21,7 @@ module.exports = {
         if(message.content.toLowerCase().includes("fluked")) { 
             message.reply("https://cdn.discordapp.com/attachments/908882016345395241/911898128448364544/You.gif")
         }
-        if(message.content = "-test") {
+        if(message.content == "-test") {
             const re = require("../levels_getting")
             message.reply(re["Aronia"].toString())
         }
