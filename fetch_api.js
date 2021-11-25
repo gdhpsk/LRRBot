c06bc41bb4fd23e9ec2195e63bcf177be05b757e
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2021-11-24 22:32:57 -0800

    Update fetch_api.js

--- fetch_api.js
+++ fetch_api.js
@@ -13,36 +13,13 @@ let lol = async () => {
     for(const key in data2) {
         var objectval = data2[key]
         var txt = [`${objectval.minimumPercent}`, undefined]
-        if(objectval.minimumPercent) {
-            txt[0] = undefined
-        }
-        var list = []
-        if(objectval.progresses) {
-            var progs = []
-            for(let i = 0; i < objectval.progresses.length; i++) {
-                progs.push({
-                   name: objectval.list[i].name,
-                   percent: objectval.list[i].percent,
-                   link: objectval.list[i].link,
-                   hertz: objectval.list[i].hertz 
-                })  
-          }
-            txt[1] = progs
-        }
-        for(let i = 0; i < objectval.list.length; i++) {
-              list.push({
-                 name: objectval.list[i].name,
-                 link: objectval.list[i].link,
-                 hertz: objectval.list[i].hertz 
-              })  
-        }
         obj.push({
             name: "Aronia",
             ytcode: objectval.ytcode,
-            minimumPercent: txt[0],
+            minimumPercent: objectval.minimumPercent,
             publisher: objectval.publisher,
             list: list,
-            progresses: txt[1]
+            progresses: objectval.progresses
         })
     }
     return obj
