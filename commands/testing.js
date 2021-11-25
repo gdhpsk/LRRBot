2be0624a3dbc5b8cd17b3f31beec8c3a59fd6091
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2021-11-24 22:41:23 -0800

    k

--- commands/testing.js
+++ commands/testing.js
@@ -7,6 +7,6 @@ module.exports = {
     .setDescription("testing"),
     async execute(interaction, Discord, client) {
         var api = await require("../fetch_api") 
-       await interaction.reply(api[0].list[0].link)
+       await interaction.reply(`${api.length}, ${api[api.length].name}`)
     }
 }
--- fetch_api.js
+++ fetch_api.js
@@ -3,15 +3,27 @@ var obj = []
 
 let lol = async () => {
     const body = {a: 1};
-    const response2 = await fetch('https://gdlrrlistcf-4.gdhpsk.repl.co/JS/mainlist.json', {
+    const response = await fetch('https://gdlrrlistcf-4.gdhpsk.repl.co/JS/mainlist.json', {
+	    method: 'post',
+	    body: JSON.stringify(body),
+	    headers: {'Content-Type': 'application/json'}
+    });
+    const data = await response.json();
+    const response2 = await fetch('https://gdlrrlistcf-4.gdhpsk.repl.co/JS/extended.json', {
 	    method: 'post',
 	    body: JSON.stringify(body),
 	    headers: {'Content-Type': 'application/json'}
     });
     const data2 = await response2.json();
+    const response3 = await fetch('https://gdlrrlistcf-4.gdhpsk.repl.co/JS/legacy.json', {
+	    method: 'post',
+	    body: JSON.stringify(body),
+	    headers: {'Content-Type': 'application/json'}
+    });
+    const data3 = await response3.json();
     
-    for(const key in data2) {
-        var objectval = data2[key]
+    for(const key in data) {
+        var objectval = data[key]
         var txt = [`${objectval.minimumPercent}`, undefined]
         obj.push({
             name: objectval.name,
@@ -20,6 +32,28 @@ let lol = async () => {
             publisher: objectval.publisher,
             list: objectval.list,
             progresses: objectval.progresses
+        })
+    }
+    for(const key in data2) {
+        var objectval = data2[key]
+        var txt = [`${objectval.minimumPercent}`, undefined]
+        obj.push({
+            name: objectval.name,
+            ytcode: objectval.ytcode,
+            minimumPercent: 100,
+            publisher: objectval.publisher,
+            list: objectval.list
+        })
+    }
+    for(const key in data3) {
+        var objectval = data3[key]
+        var txt = [`${objectval.minimumPercent}`, undefined]
+        obj.push({
+            name: objectval.name,
+            ytcode: objectval.ytcode,
+            minimumPercent: 100,
+            publisher: objectval.publisher,
+            list: objectval.list
         })
     }
     return obj
