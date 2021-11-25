f541feff00351a191c13295823a61b48a9d2dcf7
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2021-11-25 13:35:48 -0800

    Update leaderboard.js

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -22,7 +22,7 @@ module.exports = {
             var gay = interaction.options.getString("user")
             var far = []
             for(let key in leaderboard) {
-                var df = point(key)
+                var df = await test(key)
                 var obj = {
                     name: key,
                     points: df
