08900bd0f8e28eaa5a7d39a7a35111026483107b
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2021-11-24 19:17:18 -0800

    k

--- fetch_api.js
+++ fetch_api.js
@@ -1,46 +1,48 @@
 const got = require("got")
 var obj = []
-var levels;
 got("https://gdlrrlistcf-4.gdhpsk.repl.co/JS/mainlist.json").then(response => {
-    levels = JSON.parse(response.body)
-})
-
-for(const key in levels) {
-    var objectval = levels[key]
-    var txt = [`${objectval.minimumPercent}`, undefined]
-    if(objectval.minimumPercent) {
-        txt[0] = undefined
-    }
-    var list = []
-    if(objectval.progresses) {
-        var progs = []
-        for(let i = 0; i < objectval.progresses.length; i++) {
-            progs.push({
-               name: objectval.list[i].name,
-               percent: objectval.list[i].percent,
-               link: objectval.list[i].link,
-               hertz: objectval.list[i].hertz 
-            })  
-      }
-        txt[1] = progs
-    }
-    for(let i = 0; i < objectval.list.length; i++) {
-          list.push({
-             name: objectval.list[i].name,
-             link: objectval.list[i].link,
-             hertz: objectval.list[i].hertz 
-          })  
-    }
-    obj.push({
-        name: "Aronia",
-        ytcode: objectval.ytcode,
-        minimumPercent: txt[0],
-        publisher: objectval.publisher,
-        list: list,
-        progresses: txt[1]
+    got("https://gdlrrlistcf-4.gdhpsk.repl.co/JS/extended.json").then(response2 => {
+        got("https://gdlrrlistcf-4.gdhpsk.repl.co/JS/legacy.json").then(response3 => {
+            var levels = JSON.parse(response.body)
+            for(const key in levels) {
+                var objectval = levels[key]
+                var txt = [`${objectval.minimumPercent}`, undefined]
+                if(objectval.minimumPercent) {
+                    txt[0] = undefined
+                }
+                var list = []
+                if(objectval.progresses) {
+                    var progs = []
+                    for(let i = 0; i < objectval.progresses.length; i++) {
+                        progs.push({
+                           name: objectval.list[i].name,
+                           percent: objectval.list[i].percent,
+                           link: objectval.list[i].link,
+                           hertz: objectval.list[i].hertz 
+                        })  
+                  }
+                    txt[1] = progs
+                }
+                for(let i = 0; i < objectval.list.length; i++) {
+                      list.push({
+                         name: objectval.list[i].name,
+                         link: objectval.list[i].link,
+                         hertz: objectval.list[i].hertz 
+                      })  
+                }
+                obj.push({
+                    name: "Aronia",
+                    ytcode: objectval.ytcode,
+                    minimumPercent: txt[0],
+                    publisher: objectval.publisher,
+                    list: list,
+                    progresses: txt[1]
+                })
+            }
+            module.exports = obj[0].name
+        })
     })
-    
-}
+})
 
 /*got("https://gdlrrlistcf-4.gdhpsk.repl.co/JS/extended.json").then(response => {
     var levels = JSON.parse(response.body)
@@ -93,6 +95,3 @@ got("https://gdlrrlistcf-4.gdhpsk.repl.co/JS/legacy.json").then(response => {
         })
     }
 })*/
-
-
-module.exports = "obj[0].name"
