59a99979e1e19a31847b90b105981f88b196a0d8
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2021-11-27 10:28:49 -0800

    Update list.js

--- commands/list.js
+++ commands/list.js
@@ -51,7 +51,7 @@ module.exports = {
         var whyudo = 0
 
         client.on('messageReactionAdd', async (reaction, user) => {
-            if (reaction.message.partial) await reaction.interaction.fetch();
+            if (reaction.message.partial) await reaction.message.fetch();
             if (reaction.partial) await reaction.fetch();
             if (user.bot) return;
             if (!reaction.message.guild) return;
