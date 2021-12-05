f00bce48fcbc621b54b7a35f98129feba0b817a2
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2021-12-04 23:04:11 -0800

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -57,7 +57,7 @@ module.exports = {
         if(message.content == "..roulette") {
             var levels = require("../JSON/levels.json")
             var array = []
-            var random = Math.random(Math.random() * Object.keys(levels).length-1)
+            var random = Math.floor(Math.random() * Object.keys(levels).length-1)
             if(!array.includes(Object.keys(levels)[random])) {
                 message.reply(`#${random+1}. ${Object.keys(levels)[random]}, you have to get ${array.length+1}%`)
                 array.push(Object.keys(levels)[random])
