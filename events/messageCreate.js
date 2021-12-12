1fbd2ffd8fad6cdba31a35497a018e1e1b6928d0
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2021-12-12 01:05:49 -0800

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -101,6 +101,7 @@ module.exports = {
                     const filter = m => m.author.id === message.client.users.cache.find(user => user.id == args[1]).id;
                     const collector = message.channel.createMessageCollector(filter, {time: 10000});
                     collector.on("collect", msg => {
+                        if(!msg.author.bot) {
                         if(msg.content == "yes") {
                              msg.channel.send(`<@${id}>, This person has approved your request`)
                             collector.stop()
@@ -110,6 +111,7 @@ module.exports = {
                         } else {
                             return msg.channel.send("Send a valid response! (either yes or no)")
                         }
+                    }
                     })
                 } else {
                     return message.reply("Please enter a valid user ID")
