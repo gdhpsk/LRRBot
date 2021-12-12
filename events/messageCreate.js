7f5167417bd97c2fb34c1603c09141d7ce9deb94
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2021-12-12 01:07:35 -0800

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -98,10 +98,9 @@ module.exports = {
                 if(message.client.users.cache.find(user => user.id == args[1])) {
                     const id = message.author.id
                     message.channel.send(`<@${message.client.users.cache.find(user => user.id == args[1]).id}>, do you want to join a roulette?`)
-                    const filter = m => m.author.id === message.client.users.cache.find(user => user.id == args[1]).id;
+                    const filter = m => m.author.id === message.client.users.cache.find(user => user.id == args[1]).id && !m.author.bot;
                     const collector = message.channel.createMessageCollector(filter, {time: 10000});
                     collector.on("collect", msg => {
-                        if(!msg.author.bot) {
                         if(msg.content == "yes") {
                              msg.channel.send(`<@${id}>, This person has approved your request`)
                             collector.stop()
@@ -111,7 +110,6 @@ module.exports = {
                         } else {
                             return msg.channel.send("Send a valid response! (either yes or no)")
                         }
-                    }
                     })
                 } else {
                     return message.reply("Please enter a valid user ID")
