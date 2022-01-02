2a10f5af41afaa3c04a2b72e5e77ab96bc2ed05a
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2022-01-02 13:15:55 -0800

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -72,16 +72,12 @@ module.exports = {
             message.channel.send("Worked Successfully")
         }
         if(cmd == "roulette") {
-            var DDFG = ""
             message.client.guilds.fetch("904222136661577758").then(guild => {
             guild.channels.fetch("904222137278169099").then(msg => {
                 msg.messages.fetch({ limit: 1 }).then(messages => {
                     var last = messages.first().content
-                    DDFG += last
-                })
-            })
-        })
-    console.log(DDFG)
+                    require("../JSON/commands.json").array = JSON.parse(last)
+                
             var karthik;
             var g;
             var levels = require("../JSON/levels.json")
@@ -241,6 +237,9 @@ module.exports = {
         message.reply("Please start the roulette!")
     }
         } 
+    })
+})
+})
     }
     }
 }
