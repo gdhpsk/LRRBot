e576d85b10af438f65fc5e6c6aa494fe86d3ab83
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2022-03-05 15:04:12 -0800

    Update roulette.js

--- schema/roulette.js
+++ schema/roulette.js
@@ -3,7 +3,7 @@ var mongoose = require("mongoose")
 var rouletteSchema = new mongoose.Schema({
     levels: Object,
     percent: Object,
-    config: Object
+    config: Object 
 })
 
 module.exports = mongoose.model("roulette", rouletteSchema)
