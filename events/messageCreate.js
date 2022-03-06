a18ec76897e6e93dee0193eab51770c522050d67
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2022-03-05 17:35:39 -0800

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -90,7 +90,6 @@ module.exports = {
             message.channel.send("Worked Successfully")
         }
         if(cmd == "roulette") {
-            if(message.author.id != "703364595321929730") return message.reply("Currently undergoing changes.")
             var real = await roulette.findById("61ff7b2fbd245cb98f6579fd")
             var object = real.levels 
         var obj = real.percent
