818322ba2b95ffde1b4d99238b975b1b80639dbc
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2022-03-27 14:41:30 -0700

    Update leaderboard.js

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -42,7 +42,7 @@ module.exports = {
             }
             }
             far.sort((a, b) => b.points - a.points)
-            console.log(far)
+            console.log(JSON.stringify(far))
            for(let i = 0; i < far.length; i++) {
                 if(far[i].name == gay) {
                     counte = `#${i+1} - `
