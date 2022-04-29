c9a91ff885da5af5b8c875730da08efaaa82a571
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2022-04-28 23:54:51 -0700

    l

--- commands/opinions.js
+++ commands/opinions.js
@@ -64,6 +64,7 @@ module.exports = {
             comments: interaction.options.getString("comments")
         }
         let levopinion = await opinionsSchema.findOne({name: interaction.options.getString("name")})
+        console.log(levopinion)
         if(!levopinion) {
              await opinionsSchema.create({_id: level.name, opinions: [object]})
         } else {
