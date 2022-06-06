86676b6549d6fcc5073d5e3f601a6fa96ca61a72
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2022-06-06 00:14:33 -0700

    p

--- commands/secretcmd.js
+++ commands/secretcmd.js
@@ -19,14 +19,34 @@ module.exports = {
                    new TextInputBuilder()
                    .setCustomId("id")
                    .setLabel("Role ID")
+                   .setPlaceholder("Put the discord role ID here")
                    .setStyle(1)
-               ]})
+                   .setRequired(true)
+               ]}),
+               new ActionRowBuilder({ components: [
+                new TextInputBuilder()
+                .setCustomId("levels")
+                .setLabel("Pack Levels")
+                .setPlaceholder("Seperate via \", \"")
+                .setStyle(1)
+                .setRequired(true)
+            ]}),
+            new ActionRowBuilder({ components: [
+                new TextInputBuilder()
+                .setCustomId("category")
+                .setLabel("Category")
+                .setPlaceholder("Legacy, Advanced, etc.")
+                .setStyle(1)
+                .setRequired(true)
+            ]}),
            ])
            await interaction.showModal(modal)
         client.on('interactionCreate', interaction => {
             if (!interaction.isModalSubmit()) return;
-            const text = interaction.fields.getTextInputValue('id');
-            interaction.reply({content: `You inputted the role ID ${text}`, ephemeral: true})
+            const id = interaction.fields.getTextInputValue('id');
+            const levels = interaction.fields.getTextInputValue('levels').split(", ");
+            const category = interaction.fields.getTextInputValue('category');
+            interaction.reply(`Here is what you inputted: ${id}, ${JSON.stringify(levels)}, ${category}`)
         });
         }
     }
