8ef30f6db7d2d5bb6c1c4757c6a1b95393f2f5b0
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2022-06-05 23:31:36 -0700

    o

--- commands/secretcmd.js
+++ commands/secretcmd.js
@@ -3,8 +3,8 @@ const { SlashCommandBuilder } = require("discord.js")
 module.exports = {
     data: new SlashCommandBuilder()
     .setName("secret")
-    .setDescription("Shhhhh"),
-    private: true,
+    .setDescription("Shhhhh")
+    .setDefaultMemberPermissions(1),
     async execute(interaction, Discord, client) {
         interaction.reply("yo")
     }
