ddaed20be9e6375660b17ffa910d180482c92fb6
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2022-07-23 23:44:38 +0530

    test

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -404,6 +404,7 @@ module.exports = {
             if(real?.levels && !ikl) {
                 number = args[0] != "skip" ? parseInt(args[0])+1 : real.levels[real.levels.length-1].percent+1
                 random = Math.floor(Math.random() * real.config.levels.length-1)
+                console.log(random)
                 let levelinfo = await levelsSchema.findOne({name: real.config.levels[random].name})
                 const embed = new Discord.EmbedBuilder() 
                 .setTitle(`#${real.config.levels[random].pos} - ${levelinfo.name} by ${levelinfo.publisher}`)
